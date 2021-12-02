22ad2d86c605f80662f47d6d51bcbc763af7e2c1
mohittk <mvk1407@gmail.com>
2021-12-02 11:07:32 +0530

    added navbar.js Landing.js

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Navbar />
+      <Landing />
+
+    </div>
+
+    </>
+   
+  )
+}
+
+export default App;
